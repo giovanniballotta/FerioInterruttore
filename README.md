ad884cef22f935b8365c4fa953b2480bf5ee714c
giovanniballotta <giovanni@giovanniballotta.com>
2021-03-27 12:41:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FerioInterruttore
+
+Developed with Unreal Engine 4
---
+++ Source/FerioInterruttore/FerioInterruttoreGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FerioInterruttoreGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FERIOINTERRUTTORE_API AFerioInterruttoreGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
